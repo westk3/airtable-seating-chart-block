74402f1e54003c649b6fe210fb8a003f84c8cf38
Katherine West <46939574+westk3@users.noreply.github.com>
2020-06-25 20:17:44 -0700

    Setup up initial input fields

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,56 @@
-import {initializeBlock} from '@airtable/blocks/ui';
-import React from 'react';
+import {
+    initializeBlock,
+    useBase,
+    useRecords,
+    useGlobalConfig,
+    TablePickerSynced,
+    Input,
+    Label,
+    FormField,
+    FieldPickerSynced,
+} from '@airtable/blocks/ui';
+import React, {useState} from 'react';
 
-function HelloWorldBlock() {
-    // YOUR CODE GOES HERE
-    return <div>Hello world 🚀</div>;
+function SeatingChartBlock() {
+
+    const base = useBase();
+
+    const globalConfig = useGlobalConfig();
+    const guestTableId = globalConfig.get('guestsTableId');
+    const relationshipFieldId = globalConfig.get('relationshipFieldId');
+
+    const guestTable = base.getTableByIdIfExists(guestTableId);
+    const relationshipField = guestTable ? guestTable.getFieldByIdIfExists(relationshipFieldId) : null;
+
+    const [numTables, setTableValue] = useState("");
+    const [numChairs, setChairValue] = useState("");
+
+    const records = useRecords(guestTable);
+
+    return <div>
+        <TablePickerSynced globalConfigKey="guestTableId" />
+        <FieldPickerSynced table={guestTable} globalConfigKey="relationshipFieldId" />
+
+        <FormField label="Number of Tables">
+            <Input value={numTables} type="number" onChange={e => setTableValue(e.target.numTables)} />
+        </FormField>
+
+        <FormField label="Number of Chairs">
+            <Input value={numChairs} type="number" onChange={e => setChairValue(e.target.numChairs)} />
+        </FormField>
+    </div>;
 }
 
-initializeBlock(() => <HelloWorldBlock />);
+//initial setup function
+
+//function to change settings later
+
+//seating automation function
+
+//pull from existing storage
+
+//show on map
+
+//rename seating chart?, drag and drop?, specific view for table?, check permissions
+
+initializeBlock(() => <SeatingChartBlock />);
