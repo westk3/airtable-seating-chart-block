96e5e7e57dcc9e9493009e6c1ac7feb192a4e1f3
Katherine West <46939574+westk3@users.noreply.github.com>
2020-06-26 19:56:48 -0700

    Updated initial settings screen and started seating chart visualization function

--- frontend/index.js
+++ frontend/index.js
@@ -10,10 +10,13 @@ import {
     FieldPickerSynced,
     Button,
     settingsButton,
+    useSettingsButton,
 } from '@airtable/blocks/ui';
 import React, {useState, useEffect} from 'react';
 
 function SeatingChartBlock() {
+
+    const [isShowingSettings, setIsShowingSettings] = useState(false);
 
     var newChartDisabled = false;
     var existingChartDisabled = false;
@@ -21,30 +24,71 @@ function SeatingChartBlock() {
     const [showNewChart, setShowNewChart] = useState(false);
     const [loadExistingChart, setLoadExistingChart] = useState(false);
 
-    return <div>
-        {!showNewChart && !loadExistingChart && <Button disabled={newChartDisabled} onClick={() => setShowNewChart(true)} icon="cog">
-            Create a New Seating Chart
-        </Button>}
+    if (isShowingSettings) {
+        return <SettingsComponent />;
+    } else if (!showNewChart && !loadExistingChart) {
+        return <div>
+            <Button disabled={newChartDisabled} onClick={() => setShowNewChart(true)} icon="cog">
+                Create a New Seating Chart
+            </Button>
 
-        {!showNewChart && !loadExistingChart && <Button disabled={existingChartDisabled} onClick={() => setLoadExistingChart(true)} icon="download">
-            Load an Existing Seating Chart
-        </Button>}
+            <Button disabled={existingChartDisabled} onClick={() => setLoadExistingChart(true)} icon="download">
+                Load an Existing Seating Chart
+            </Button>
 
-        {showNewChart && !loadExistingChart && <NewSeatingChart />}
-
-        {loadExistingChart && !showNewChart && <LoadExistingSeatingChart />}
-
-        //home page navigation
-
-    </div>;
+            <Button
+                onClick={() => setIsShowingSettings(true)}
+                icon="settings"
+                aria-label="Settings">Settings
+            </Button>
+        </div>;
+    } else if (showNewChart && !loadExistingChart) {
+        return <NewSeatingChart />;
+    } else if (loadExistingChart && !showNewChart) {
+        return <LoadExistingSeatingChart />;
+    } else {
+        return <div>ERROR, try refreshing the page.</div>;
+    }
 }
 
-// initial setup function
-    //create seating chart save table, with link to guest table, seating chart name, table number, chair number
+// settings component
+function SettingsComponent() {
+    //not need settings until want to click load existing or until try to save old?
 
-// edit settings later
-function editSettings() {
+    const base = useBase();
 
+    const globalConfig = useGlobalConfig();
+
+    const dataTableId = globalConfig.get('dataTableId');
+    const dataTable = base.getTableByIdIfExists(dataTableId);
+
+    const chartNameFieldId = globalConfig.get('chartNameFieldId');
+    const guestNameFieldId = globalConfig.get('guestNameFieldId');
+    const tableNumFieldId = globalConfig.get('tableNumFieldId');
+    const chairNumFieldId = globalConfig.get('chairNumFieldId');
+
+    return <div>
+
+        <Label htmlFor="data-table-picker">Select table to store seating chart data</Label>
+        <TablePickerSynced id="data-table-picker" globalConfigKey="dataTableId" />
+
+        //get unique identifier field for each record?
+
+        //need seating chart unique identifier??
+        <Label htmlFor="chart-name-field-picker">Select field that contains the seating chart name</Label>
+        <FieldPickerSynced id="chart-name-field-picker" table={dataTable} globalConfigKey="chartNameFieldId" />
+
+        //check unique and that is lookup?
+        <Label htmlFor="guest-name-field-picker">Select field that contains the guest lookup field</Label>
+        <FieldPickerSynced id="guest-name-field-picker" table={dataTable} globalConfigKey="guestNameFieldId" />
+
+        <Label htmlFor="table-num-field-picker">Select field that contains the table number</Label>
+        <FieldPickerSynced id="table-num-field-picker" table={dataTable} globalConfigKey="tableNumFieldId" />
+
+        <Label htmlFor="chair-num-field-picker">Select field that contains the chair number</Label>
+        <FieldPickerSynced id="chair-num-field-picker" table={dataTable} globalConfigKey="chairNumFieldId" />
+
+    </div>;
 }
 
 // new seating chart set-up function
@@ -62,15 +106,18 @@ function NewSeatingChart() {
     const [numTables, setTableValue] = useState("");
     const [numChairs, setChairValue] = useState("");
 
+    // disable Generate Seating Chart button until fields are filled
+    const buttonDisabled = numTables <= 0 || numTables == null || numChairs <= 0 ||  numChairs == null || guestTable == null || relationshipField == null;
+
     // note if number of guests exceeds capacity given
 
     var errorMessage = "";
 
-    if (numTables != null && numChairs !=null && useRecords(guestTable).length > numTables * numChairs) {
+    var guestTableLength = useRecords(guestTable).length;
+
+    if (!buttonDisabled && numTables != null && numChairs != null && guestTableLength > numTables * numChairs) {
         errorMessage = "You do not have enough capacity to seat all your guests. If you click Generate Seating Chart, a chart will generate using the first " + numTables * numChairs + " people on your list.";
     }
-
-    const buttonDisabled = numTables == null || numChairs == null || guestTable == null || relationshipField == null;
 
     return <div>
         <Label htmlFor="guest-table-picker">Select table that contains guest data</Label>
@@ -88,7 +135,6 @@ function NewSeatingChart() {
 
         {errorMessage}
 
-        //disable generate chart button until fields are filled
         <Button onClick={() => seatingAutomation()} disabled={buttonDisabled} icon="personalAuto">
             Generate Seating Chart
         </Button>
@@ -110,7 +156,10 @@ function LoadExistingSeatingChart() {
 
     const dataTable = base.getTableByIdIfExists(dataTableId);
 
-    const records = useRecords(table);
+    const records = useRecords(dataTable);
+
+    //filter records by correct chart, then sort by table number and chair number
+    //and pass to render function with number of tables and chairs
 
     //use settings for correct fields (update settings if errors)
 
@@ -135,23 +184,39 @@ function LoadExistingSeatingChart() {
 
 // Create seating chart automatic backend function
 
-function seatingAutomation(records) {
+function seatingAutomation(records, numTables, numChairs) {
     console.log("Automated seating chart");
 
     // if no relationships, seat at random table with other people with no relationships
 
     // add loading icon
 
-    // call seating chart map creation
-    const chart = renderSeatingChart(records);
+    // sort records by table number and then by seat
+
+    // call seating chart map creation with number of tables and chairs
+    const chart = renderSeatingChart(records, numTables, numChairs);
 
     return <div>{chart}</div>;
 }
 
 // Render the seating chart
 
-function renderSeatingChart(records) {
+function renderSeatingChart(records, numTables, numChairs) {
 
+    var chart = "";
+
+    for (var i = 0; i < numTables; i++) {
+
+        chart += "<h2>Table " + i + 1 + "</h2><ol>";
+
+        for (var j = 0; j < numChairs; j++) {
+            chart += "<li>" + record[i * j].name + "</li>";
+        }
+
+        chart += "</ol>";
+
+    }
+    // sort by table and then
 }
 
 // Save seating chart data to file
